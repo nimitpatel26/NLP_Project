87c675e8cbcac54453c36583a17622667521b745
jacob <jacob@jacob-laptop.home>
2019-11-20 14:20:58 -0500

    Parallelize parsing of dataset files

--- Parsing/ARXIV/parse.py
+++ Parsing/ARXIV/parse.py
@@ -1,21 +1,25 @@
 from os import listdir
 from os.path import isfile, join
+from multiprocessing import Pool, Process
 import xml.etree.ElementTree as ET
 import pickle
 
-#path to dataset, I wouldn't change it since the gitignore might include it 
+# path to dataset, I wouldn't change it since the gitignore might include it 
 datasetPath = "./dataset"
 
-#Get all files in the dataset path
+# Get all files in the dataset path
+
 onlyFiles = [f for f in listdir(datasetPath) if isfile(join(datasetPath, f))]
 
-#Different dict for grouping papers differently
-bySpecialization = {}
-byAllCategory = {}
-byFirstCategory = {}
 
-for name in onlyFiles:
-    
+# worker function to parse a dataset file given the file name 
+
+def parseFile(name):
+
+    global datasetPath
+
+    out = []
+
     file = open(datasetPath+"/"+name,mode="r",encoding="UTF-8")
     
     tree = ET.parse(file)
@@ -37,47 +41,63 @@ for name in onlyFiles:
     # </record>
 
     # Get all record tags for the file, therefore getting tags for all paper records
+
     allPapers = tree.getroot().find("ListRecords").findall("record")
     
     for paper in allPapers:
         
+        metaPrefix = "{http://arxiv.org/OAI/arXiv/}"
+        info = 0
+        specialization = 0
+        categories = 0
+        abstract = 0
+
         try:
             
             # get all the fields we will need for classification out of the record
+
             metaPrefix = "{http://arxiv.org/OAI/arXiv/}"
             info = paper.find("metadata").find(metaPrefix+"arXiv")
             specialization = paper.find("header").find("setSpec").text.split(":")[0]
             categories = info.find(metaPrefix+"categories").text.split(" ")
             abstract = info.find(metaPrefix+"abstract").text
-            
-            # group into specialization, first category, and all categories
-
-            if(not specialization in bySpecialization):
-                bySpecialization[specialization] = []
-            
-            bySpecialization[specialization].append(abstract)
-            
-            if(not categories[0] in byFirstCategory):
-                byFirstCategory[categories[0]] = []
-            
-            byFirstCategory[categories[0]].append(abstract)
-            
+        
             for category in categories:
-            
-                if(not category in byAllCategory):
-                    byAllCategory[category] = []
-            
-                byAllCategory[category].append(abstract)
+                
+                out.append([category,abstract])
         
         # This would occur if the OAI harvest was interrupted while collecting data
+
         except:
+            
             print("Record " +str(paper.find("header").find("identifier").text) + " in " +str(name) + " does not contain required info")
 
-with open("arXivFirstCategory", 'wb') as handle:
-    pickle.dump(byFirstCategory, handle, protocol=pickle.HIGHEST_PROTOCOL)
-with open("arXivAllCategory", 'wb') as handle:
-    pickle.dump(byAllCategory, handle, protocol=pickle.HIGHEST_PROTOCOL)
-with open("arXivSpecialization", 'wb') as handle:
-    pickle.dump(bySpecialization, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    return out
 
+# Pool of processes for parsing the files 
+fileParsingPool = Pool()
 
+# For each filename in only files, pass that into parseFile and run as a separate process in the fileParsingPool, allowing parallelism
+
+map = fileParsingPool.map_async(parseFile,onlyFiles)
+
+# Close off the parsing process pool and then join it to the main process so we wait until all results retrieved
+
+fileParsingPool.close()
+
+fileParsingPool.join()
+
+number = 0
+
+result = []
+
+# Concatenate each file's parsed results onto the results array, this one can take a while since it's single thread
+
+for res in map.get(timeout=0):
+
+    result = result + res
+    
+
+with open("arXivAll.p", 'wb') as handle:
+    
+    pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
